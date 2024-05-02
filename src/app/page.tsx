2703d2f330f123e3c809102ba1de924a403973f2
Sandip Gyawali <sandipgyawali10@gmail.com>
2024-05-02 12:49:30 +0545

    navigate option page to different route page integration

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,22 +4,91 @@ export default function Home() {
       <p className="text-center text-4xl font-semibold">
         Welcome to Url-Shortner
       </p>
-      <a
-        href="http://localhost:3000/url"
-        className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-        // target="_blank"
-        rel="noopener noreferrer"
-      >
-        <h1 className="mb-3 text-2xl font-semibold">
-          Url-Shortner{" "}
-          <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-            -&gt;
-          </span>
-        </h1>
-        <p className="m-0 max-w-[30ch] text-sm opacity-50">
-          Navigate to http://localhost:3000/url&nbsp;for all existing url!
-        </p>
-      </a>
+      <div className="grid grid-cols-2 gap-8">
+        <a
+          href="http://localhost:3000/url"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          // target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h1 className="mb-3 text-2xl font-semibold">
+            Get All Urls{" "}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
+          </h1>
+          <p className="m-0 max-w-[30ch] text-sm">
+            Navigate to{" "}
+            <span className="text-violet-500">
+              http://localhost:3000/url&nbsp;
+            </span>
+            for all existing url!
+          </p>
+        </a>
+
+        <a
+          href="http://localhost:3000/get_specific_url"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          // target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h1 className="mb-3 text-2xl font-semibold">
+            Get Specific Url{" "}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
+          </h1>
+          <p className="m-0 max-w-[30ch] text-sm">
+            Navigate to{" "}
+            <span className="text-violet-500">
+              http://localhost:3000/get_specific_url&nbsp;
+            </span>
+            for all existing url!
+          </p>
+        </a>
+
+        <a
+          href="http://localhost:3000/delete"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          // target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h1 className="mb-3 text-2xl font-semibold">
+            Delete Specific Url{" "}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
+          </h1>
+          <p className="m-0 max-w-[30ch] text-sm">
+            Navigate to{" "}
+            <span className="text-violet-500">
+              http://localhost:3000/delete&nbsp;
+            </span>
+            for all existing url!
+          </p>
+        </a>
+
+        <a
+          href="http://localhost:3000/insert"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          // target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h1 className="mb-3 text-2xl font-semibold">
+            Insert Url{" "}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
+          </h1>
+          <p className="m-0 max-w-[30ch] text-sm">
+            Navigate to{" "}
+            <span className="text-violet-500">
+              http://localhost:3000/insert&nbsp;
+            </span>
+            for all existing url!
+          </p>
+        </a>
+      </div>
     </main>
   );
 }
